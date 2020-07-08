fa92979523e64f6bda058d3d32207b793e41485b
Brian Park <park-brian@users.noreply.github.com>
2020-07-08 19:18:18 -0400

    eliminate double slashes in s3 key

--- server/app-worker.js
+++ server/app-worker.js
@@ -106,7 +106,8 @@ async function processMessage(message) {
         const outputFilePath = path.resolve(processingFolder, outputFileName);
         // const outputS3Key = `${config.s3.outputPrefix}${params.id}/${outputFileName}`;
         let outputPath = path.dirname(key.replace(config.s3.inputPrefix, '')); // dirname strips ending slash
-        outputPath = outputPath.replace(/^(\.*\/*)+/, '') + '/';    // strip leading relative directory paths
+        outputPath = outputPath.replace(/^(\.*\/*)+/, ''); // strip leading relative directory paths
+        if (outputPath.length) outputPath += '/'; // add trailing slash if needed
         const outputS3Key = `${config.s3.outputPrefix}${outputPath}${outputFileName}`;
         fs.mkdirSync(processingFolder, {recursive: true});
         
