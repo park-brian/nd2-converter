0a99b5e6ae6a7de5a0629a2d75292b654f5d56f7
Brian Park <park-brian@users.noreply.github.com>
2020-07-09 11:50:58 -0400

    specify number of workers as argument

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 docker build -t nd2-converter -f Dockerfile .
 
 docker rm -f nd2-converter
-docker rm -f nd2-converter-worker
-
 docker run -d -p 9070:9000 --name nd2-converter --restart always nd2-converter npm start
-docker run -d --name nd2-converter-worker --restart always nd2-converter npm run start-worker
+
+for (( i=1; i<=${1:-4}; i++ ))
+do  
+    docker rm -f nd2-converter-worker-$i
+    docker run -d --name nd2-converter-worker-$i --restart always nd2-converter npm run start-worker
+done
