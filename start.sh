003c7a82405627998c4fc905d9bbaa01d920207b
Brian Park <park-brian@users.noreply.github.com>
2020-07-10 01:04:47 -0400

    update default port

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 docker build -t nd2-converter -f Dockerfile .
 
 docker rm -f nd2-converter
-docker run -d -p 9070:9000 --name nd2-converter --restart always nd2-converter npm start
+docker run -d -p 80:10000 --name nd2-converter --restart always nd2-converter npm start
 
-for (( i=1; i<=${1:-4}; i++ ))
+for (( i=1; i<=${1:-1}; i++ ))
 do  
     docker rm -f nd2-converter-worker-$i
     docker run -d --name nd2-converter-worker-$i --restart always nd2-converter npm run start-worker
