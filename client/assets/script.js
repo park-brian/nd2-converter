8873288160d7ccae1eac187ecd80d2d2cb31108f
Brian Park <park-brian@users.noreply.github.com>
2020-07-15 03:47:46 -0400

    add status page

--- client/assets/script.js
+++ client/assets/script.js
@@ -30,4 +30,57 @@ $(function () {
             }
           });
     });
+
+    // initialize job status table
+    $.getJSON('status', function(data) {
+        console.log('data', data);
+        
+        $('#job-status-table').dataTable({
+            data: data,
+            columns: [
+                {
+                    data: 'status',
+                    title: 'Status',
+                },
+                {
+                    data: 'inputKey',
+                    title: 'Input Key',
+                },
+                {
+                    data: 'outputKey',
+                    title: 'Output Key',
+                },
+                {
+                    data: 'elapsedTime',
+                    title: 'Elapsed Time (s)',
+                },
+                {
+                    data: 'fileSize',
+                    title: 'File Size',
+                    render: function(data, type, row) {
+                        if (type !== 'display' && type !== 'filter')
+                            return data;
+                        if (data < 1024) {
+                            return (+data) + ' b';
+                        } else if (data < 1024 * 1024) {
+                            return (+data/1024).toFixed(2) + ' KiB';
+                        } else if (data < 1024 * 1024 * 1024) {
+                            return (+data/1024/1024).toFixed(2) + ' MiB';
+                        } else {
+                            return (+data/1024/1024/1024).toFixed(2) + ' GiB';
+                        }
+                    }
+                },
+                {
+                    data: 'convertedAt',
+                    title: 'Converted At',
+                },
+            ],
+            dom: "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+            "<'table-responsive mb-3'tr>" +
+            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+        })
+        $('#status').DataTable();
+    })
+
 });
